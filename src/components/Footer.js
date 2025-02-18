59fe6d4743f3b95450bd9c9c0917f5020c61c44f
devansh bhawsar <bhawsard75@gmail.com>
2025-02-18 19:36:34 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,34 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer" style={{ backgroundColor: "#000" }}>
       <Container>
         <Row className="align-items-center">
-          <MailchimpForm />
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          <Col size={12} sm={6} className="text-center text-sm-start">
+            <p
+              style={{
+                fontSize: "2.0rem", // Increase font size
+                fontWeight: "bold", // Make the text bold
+                color: "#f8f8f8", // Light color for dark background
+                marginBottom: "20px", // Add some margin below
+                letterSpacing: "1px", // Slightly increase letter spacing
+              }}
+            >
+              Thanks For Visiting
+            </p>
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a
-                href="https://linkedin.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={navIcon1} alt="LinkedIn" />
-              </a>
-              <a
-                href="https://facebook.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={navIcon2} alt="Facebook" />
-              </a>
-              <a
-                href="https://instagram.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={navIcon3} alt="Instagram" />
-              </a>
+          <Col size={12} sm={6} className="d-flex justify-content-end">
+            <div>
+              <p className="contact-text">
+                Contact me at:{" "}
+                <a className="email-link" href="mailto:bhawsard75@gmail.com">
+                  bhawsard75@gmail.com
+                </a>
+              </p>
+              <p>Copyright 2025. All Rights Reserved</p>
             </div>
-            <p>Copyright 2025. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,5 @@
-import React from "react";
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -35,9 +32,6 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
