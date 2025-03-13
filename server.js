c79eeef096d2d9c68ef8e3d4a765ef9a6d3ba943
devansh bhawsar <bhawsard75@gmail.com>
2025-03-13 10:36:19 +0530

    new commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
